5d83ef8ed961d59207623f1034c8a91486335af3
lqy <18810429058@163.com>
2021-12-06 17:40:27 +0800

    nacos配置

---
+++ config_nacos/src/main/java/com/lqy/config/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.lqy.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * config启动类
+ *
+ * @ClassName: ConfigApplication
+ * @Author: liqiuyue
+ * @Date: 2021-12-06
+ */
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
